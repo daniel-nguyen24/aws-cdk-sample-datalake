f706ba7adc999ace3b8f6c5c73ebf8810d7c5fcd
Daniel Nguyen <xuananh.nguyenha@gmail.com>
2022-06-22 16:10:29 +1000

    add components to the stack and relevant tests

--- tests/unit/test_wttr_in_data_stack.py
+++ tests/unit/test_wttr_in_data_stack.py
@@ -1,15 +1,56 @@
-import aws_cdk as core
-import aws_cdk.assertions as assertions
-
+from aws_cdk import (
+    Stack,
+    aws_s3,
+    assertions,
+    App
+)
 from wttr_in_data.wttr_in_data_stack import WttrInDataStack
+import pytest
 
-# example tests. To run these tests, uncomment this file along with the example
-# resource in wttr_in_data/wttr_in_data_stack.py
-def test_sqs_queue_created():
-    app = core.App()
-    stack = WttrInDataStack(app, "wttr-in-data")
-    template = assertions.Template.from_stack(stack)
 
-#     template.has_resource_properties("AWS::SQS::Queue", {
-#         "VisibilityTimeout": 300
-#     })
+def test_s3_ingest_bucket_created():
+    app = App()
+
+    test_stack = WttrInDataStack(app, "TestStack")
+
+    template = assertions.Template.from_stack(test_stack)
+
+    template.resource_count_is('AWS::S3::Bucket', 1)
+
+    template.has_resource('AWS::S3::Bucket', {
+        'DeletionPolicy': 'Delete',
+        'UpdateReplacePolicy': 'Delete'
+    })
+
+    template.has_resource_properties('AWS::S3::Bucket', {
+        'BucketName': 'wttr-in-data-ingest',
+        'VersioningConfiguration': {
+            'Status': 'Enabled'
+        }
+    })
+
+
+def test_lambda_function_created():
+    app = App()
+
+    test_stack = WttrInDataStack(app, "TestStack")
+
+    template = assertions.Template.from_stack(test_stack)
+
+    template.resource_count_is('AWS::Lambda::Function', 1)
+
+    env_capture = assertions.Capture()
+
+    template.has_resource_properties('AWS::Lambda::Function',
+                                     {
+                                         'Handler': 'get_data.handler',
+                                         'Runtime': 'python3.7',
+                                         'Timeout': 10,
+                                         'Environment': env_capture
+                                     })
+
+    assert env_capture.as_object() == {
+        'Variables': {
+            'INGEST_BUCKET': {'Ref': 'IngestBucket2B3522FA'}
+        }
+    }
--- wttr_in_data/wttr_in_data_stack.py
+++ wttr_in_data/wttr_in_data_stack.py
@@ -1,19 +1,54 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_s3,
+    aws_lambda,
+    aws_events,
+    RemovalPolicy,
+    Duration,
+    aws_events_targets
 )
 from constructs import Construct
+
 
 class WttrInDataStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        # Create S3 Ingest bucket
+        ingest_bucket = aws_s3.Bucket(
+            self, "IngestBucket",
+            bucket_name="wttr-in-data-ingest",
+            versioned=True,
+            removal_policy=RemovalPolicy.DESTROY
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "WttrInDataQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # Create lambda function to obtain data from wttr.in
+        get_data_lambda = aws_lambda.Function(
+            self, 'GetDataLambda',
+            runtime=aws_lambda.Runtime.PYTHON_3_7,
+            code=aws_lambda.Code.from_asset('lambda'),
+            handler='get_data.handler',
+            timeout=Duration.seconds(10),
+            environment={
+                'INGEST_BUCKET': ingest_bucket.bucket_name
+            }
+        )
+
+        # Grant write permission to created lambda function
+        ingest_bucket.grant_write(get_data_lambda)
+
+        # Create event rule to trigger lambda function every hour
+        extract_rule = aws_events.Rule(
+            self, "hourlyRule",
+            schedule=aws_events.Schedule.cron(
+                minute='0',
+                hour='*',
+                week_day='*',
+                month='*',
+                year='*'
+            )
+        )
+
+        extract_rule.add_target(
+            aws_events_targets.LambdaFunction(get_data_lambda))
